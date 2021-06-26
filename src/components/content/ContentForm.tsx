2e21761c577a0fd45babf6996929e3397d3dfa45
Artur Zaczek <artur200212@interia.pl>
2021-06-26 19:14:46 +0200

    bug fix with ContentForm and StoreContext

--- src/components/content/ContentForm.tsx
+++ src/components/content/ContentForm.tsx
@@ -4,32 +4,64 @@ import { StoreContex } from '../../store/StoreProvider';
 
 import "./ContentForm.scss"
 
-const handleReset = ()=>{
-
-}
-
 
 
 const ContentForm = () => {
-    const {price, setPrice,thing, setThing,buyDate, setBuyDate} = useContext(StoreContex)
-    console.log(typeof({}))
+    const { price, setPrice, thing, setThing, buyDate, setBuyDate } = useContext(StoreContex)
+    // console.log(Date())
+
+    const handlePrice = (event: any) => {
+        //   console.log(event.target.value)
+        const priceValue = event?.target.value
+        setPrice(priceValue)
+        // console.log(priceValue);
+    }
+
+    const handleThing = (event: any) => {
+        setThing(event.target.value)
+    }
+    const handleBuyDate = (event: any) => {
+        setBuyDate(event.target.value)
+    }
+
+    const resetFormValues = () => {
+        setPrice('');
+        setThing('');
+        setBuyDate('')
+    }
+
+    const handleSubmit = () => {
+        const objectThing = {
+            price: price,
+            thing,
+            date: buyDate,
+        }
+        console.log(objectThing)
+        resetFormValues()
+    }
+
+    const handleReset = () => {
+        resetFormValues()
+        console.log('reset')
+    }
+
     return (
         <div className='divForm'>
-            <form className='form' method='post'  >
+            <form className='form' method='post' >
                 <label className='label'>
                     Cena:
-                    <input type="number" value={price}/>
+                    <input onChange={handlePrice} type="number" value={price} placeholder="cena za rzeczy... np: 50" /> zł
                 </label>
                 <label className='label'>
                     Rzecz:
-                    <input type="text" value={thing}/>
+                    <input onChange={handleThing} type="text" value={thing} placeholder="Zakup... np: Paliwo" />
                 </label>
                 <label className='label'>
                     Data:
-                    <input type="date" value={buyDate} />
+                    <input onChange={handleBuyDate} type="date" value={buyDate} />
                 </label>
             </form>
-            <button className='submitBtn' type='submit'>Zapisz</button>
+            <button onClick={handleSubmit} className='submitBtn' type='submit'>Zapisz</button>
             <button onClick={handleReset} className='btn' type='button'>Anuluj</button>
         </div>
 
--- src/store/StoreProvider.tsx
+++ src/store/StoreProvider.tsx
@@ -1,26 +1,25 @@
 import * as React from 'react';
-import { useState,createContext } from 'react';
+import { useState, createContext } from 'react';
 
-export const StoreContex = createContext<Variables>(null)
-  
-export type Variables = {
-    null,
-    price: number,
-    setPrice: number,
-    thing: string,
-    setThing: string,
-    buyDate: string,
-    setBuyDate: string,
-}
+export const StoreContex = createContext<null | any>(null)
+
+// export type Variables = {
+//     price: any,
+//     setPrice: number | null,
+//     thing: string,
+//     setThing: string | null,
+//     buyDate: string,
+//     setBuyDate: string | null,
+// }
 
 
-const StoreProvider = ({children}:any) =>{
-    
-    const [price, setPrice] = useState(50);
-    const [thing, setThing] = useState('Paliwo');
-    const [buyDate, setBuyDate] = useState('2001-09-08');
-    return(
-        <StoreContex.Provider value ={{price, setPrice,thing, setThing, buyDate, setBuyDate}}>
+const StoreProvider = ({ children }: any) => {
+
+    const [price, setPrice] = useState<number>(Number(''));
+    const [thing, setThing] = useState<string>('');
+    const [buyDate, setBuyDate] = useState<string>('');
+    return (
+        <StoreContex.Provider value={{ price, setPrice, thing, setThing, buyDate, setBuyDate }}>
             {children}
         </StoreContex.Provider>
     )
