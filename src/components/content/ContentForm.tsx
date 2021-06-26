e203dd3fb00854c5998dcf439802fec62fbe5205
Artur Zaczek <artur200212@interia.pl>
2021-06-26 16:05:14 +0200

    few problems, aplication don't work

--- src/components/content/ContentForm.tsx
+++ src/components/content/ContentForm.tsx
@@ -1,26 +1,36 @@
 import * as React from 'react';
+import { useContext } from 'react';
+import { StoreContex } from '../../store/StoreProvider';
 
 import "./ContentForm.scss"
 
+const handleReset = ()=>{
+
+}
+
+
+
 const ContentForm = () => {
+    const {price, setPrice,thing, setThing,buyDate, setBuyDate} = useContext(StoreContex)
+    console.log(typeof({}))
     return (
         <div className='divForm'>
             <form className='form' method='post'  >
-                <label>
+                <label className='label'>
                     Cena:
-                    <input type="number" />
+                    <input type="number" value={price}/>
                 </label>
-                <label>
+                <label className='label'>
                     Rzecz:
-                    <input type="text" />
+                    <input type="text" value={thing}/>
                 </label>
-                <label>
+                <label className='label'>
                     Data:
-                    <input type="date" />
+                    <input type="date" value={buyDate} />
                 </label>
             </form>
             <button className='submitBtn' type='submit'>Zapisz</button>
-            <button className='submitBtn' type='button'>Anuluj</button>
+            <button onClick={handleReset} className='btn' type='button'>Anuluj</button>
         </div>
 
     )
--- src/store/StoreProvider.tsx
+++ src/store/StoreProvider.tsx
@@ -1,18 +1,29 @@
 import * as React from 'react';
 import { useState,createContext } from 'react';
 
-export const StoreContex = createContext({})
+export const StoreContex = createContext<Variables>(null)
   
+export type Variables = {
+    null,
+    price: number,
+    setPrice: number,
+    thing: string,
+    setThing: string,
+    buyDate: string,
+    setBuyDate: string,
+}
 
 
 const StoreProvider = ({children}:any) =>{
     
-    const [values, setValues] = useState();
+    const [price, setPrice] = useState(50);
+    const [thing, setThing] = useState('Paliwo');
+    const [buyDate, setBuyDate] = useState('2001-09-08');
     return(
-        <StoreContex.Provider value ={{values, setValues}}>
+        <StoreContex.Provider value ={{price, setPrice,thing, setThing, buyDate, setBuyDate}}>
             {children}
         </StoreContex.Provider>
     )
 }
 
-export default StoreProvider
+export default StoreProvider;
