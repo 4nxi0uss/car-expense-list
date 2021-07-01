d8c9fc3f69ea9d112cc5be14f5f8a8849c6070f4
Artur Zaczek <artur200212@interia.pl>
2021-07-01 13:10:42 +0200

    repair form grid style, problem with .env.local

--- src/components/FIrebaseUtility/Firebase.ts
+++ src/components/FIrebaseUtility/Firebase.ts
@@ -1,16 +1,18 @@
 import { initializeApp } from "firebase/app"
 import { getDatabase } from "firebase/database"
 
-const config = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID
-  };
+const {REACT_APP_API_KEY,REACT_APP_AUTH_DOMAIN,REACT_APP_DATABASE_URL,REACT_APP_PROJECT_ID,REACT_APP_STORAGE_BUCKET,REACT_APP_MESSAGING_SENDER_ID,REACT_APP_APP_ID} =process.env
 
-  const firebaseApp = initializeApp(config);
-  export const database = getDatabase();
-  export default firebaseApp;
+const firebaseConfig = {
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN ,
+  databaseURL: REACT_APP_DATABASE_URL,
+  projectId: REACT_APP_PROJECT_ID ,
+  storageBucket: REACT_APP_STORAGE_BUCKET ,
+  messagingSenderId: REACT_APP_MESSAGING_SENDER_ID ,
+  appId: REACT_APP_APP_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+export const database = getDatabase();
+export default firebaseApp;
--- src/components/content/ContentForm.tsx
+++ src/components/content/ContentForm.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
-import { useContext } from 'react';
-import { StoreContex } from '../../store/StoreProvider';
+// import { useContext } from 'react';
+// import { StoreContex } from '../../store/StoreProvider';
+
+import { nanoid } from 'nanoid';
 
 import { ref, set } from "firebase/database"
 // import { onValue } from "firebase/database"
@@ -9,11 +11,15 @@ import { database } from '../FIrebaseUtility/Firebase';
 
 import "./ContentForm.scss"
 // import { useEffect } from 'react';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 const ContentForm = () => {
     // const [dataValue, setDataValue] = useState<object>()
-    const { price, setPrice, thing, setThing, buyDate, setBuyDate, carBrand, setCarBrand } = useContext(StoreContex)
+    const [price, setPrice] = useState<number>(Number(''));
+    const [thing, setThing] = useState<string>('');
+    const [buyDate, setBuyDate] = useState<string>('');
+    const [carBrand, setCarBrand] = useState<string>('');
+    // const { price, setPrice, thing, setThing, buyDate, setBuyDate, carBrand, setCarBrand } = useContext(StoreContex)
     let objectThing = {
         price,
         thing,
@@ -21,17 +27,19 @@ const ContentForm = () => {
         carBrand,
     }
 
+
     // useEffect(() => {
     //     const test = ref(database, '/ToDo')
     //     onValue(test, (snapshot) => { setDataValue(snapshot.val()) })
     // }, [database])
 
-    const uploadData = (price: number, thing: Text, buyDate: Text, carBrand: Text) => {
-        set(ref(database, 'ToDo/55'), {
+    const uploadData = (price: number, thing: string, buyDate: string, carBrand: string) => {
+        set(ref(database, `ToDo/${nanoid()}`), {
             price,
             thing,
             date: buyDate,
-            carBrand
+            carBrand,
+            createDate: new Date().toUTCString()
         });
     }
 
@@ -50,13 +58,14 @@ const ContentForm = () => {
     }
 
     const resetFormValues = () => {
-        setPrice('');
+        setPrice(Number(''));
         setThing('');
         setBuyDate('')
         setCarBrand('')
     }
 
-    const handleSubmit = () => {
+    const handleSubmit = (e:any) => {
+        e.preventDefault()
         objectThing = {
             price,
             thing,
@@ -72,7 +81,8 @@ const ContentForm = () => {
         setCarBrand(event.target.value)
     }
 
-    const handleReset = () => {
+    const handleReset = (e:any) => {
+        e.preventDefault()
         resetFormValues()
         console.log('reset')
     }
@@ -82,11 +92,11 @@ const ContentForm = () => {
             <form className='form' method='post' onSubmit={handleSubmit}>
                 <label className='label'>
                     Cena:
-                    <input onChange={handlePrice} type="number" value={price === 0 ? "" : price} placeholder="cena za rzeczy... np: 50 zł" required={true} /> zł
+                    <input onChange={handlePrice} type="number" value={price === 0 ? "" : price} placeholder="cena za rzeczy... np: 50 zł" required /> zł
                 </label>
                 <label className='label'>
                     Rzecz:
-                    <input onChange={handleThing} type="text" value={thing} placeholder="Zakup... np: Paliwo" required={true} />
+                    <input onChange={handleThing} type="text" value={thing} placeholder="Zakup... np: Paliwo" required />
                 </label>
                 <label className='label'>
                     Data:
@@ -94,16 +104,17 @@ const ContentForm = () => {
                 </label>
                 <label className='label' >
                     Auto:
-                    <select onChange={handleCarModel} name="auto" id="auto" required value={carBrand}>
-                        <option value="-"> - Wybierz -</option>
+                    <select onChange={handleCarModel} name="auto" id="auto"  value={carBrand} required={carBrand === 'none'?true:false}>
+                        <option value='none'  > - Wybierz -</option>
                         <option value="ford">Ford</option>
                         <option value="hyundai">Hyundai</option>
                         <option value="volkswagen">Volkswagen</option>
                     </select>
                 </label>
+                <button type='submit' className='submitBtn' >Zapisz</button>
+                <button onClick={handleReset} className='btn' type='button'>Anuluj</button>
             </form>
-            <button type='submit' className='submitBtn' >Zapisz</button> {/*i must repair form, buttons must be in form marker. addicionaly i must change a style file*/}
-            <button onClick={handleReset} className='btn' type='button'>Anuluj</button>
+            
         </div>
 
     )
